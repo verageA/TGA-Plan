183a09050e3a2cab2c14bd709c28c007d8ae2bf7
Marcel Hockmann <marcelhockmann@yahoo.de>
2025-07-10 00:56:06 +0200

    Initial Commit

---
+++ TGA Plan/TGA_PlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  TGA_PlanApp.swift
+//  TGA Plan
+//
+//  Created by Marcel Hockmann on 10.07.25.
+//
+
+import SwiftUI
+
+@main
+struct TGA_PlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TGA PlanTests/TGA_PlanTests.swift
@@ -0,0 +1,17 @@
+//
+//  TGA_PlanTests.swift
+//  TGA PlanTests
+//
+//  Created by Marcel Hockmann on 10.07.25.
+//
+
+import Testing
+@testable import TGA_Plan
+
+struct TGA_PlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
